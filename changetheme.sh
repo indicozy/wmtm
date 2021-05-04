9ee5312a9c08bc134dbc82e64c60eaaa0f30fd0b
Berkut <berkut@hp-laptop.localdomain>
2021-05-05 02:49:02 +0600

    Script Update, not Reproducible for other PCs.

--- changetheme.sh
+++ changetheme.sh
@@ -1,2 +1,17 @@
-killall waybar
-killall hybridbar
+#! /bin/bash
+if [ $# -eq 1 ]
+then
+	cd ~/sway-dotfiles-script/configs
+	if [ -d "$1" ]
+	then
+		cd $1
+		cp -r config/* ~/.config
+		killall hybridbar
+		killall waybar
+		swaymsg reload
+	fi
+else
+	echo "No arguments given."
+
+fi
+
