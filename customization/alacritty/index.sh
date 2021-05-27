c5cb4eb90084765ece99887973d148196faf703a
Berkut <berkut@hp-laptop.localdomain>
2021-05-28 01:08:59 +0600

    added stupid, buggy alacritty customization

---
+++ customization/alacritty/index.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# Script for changing themes of sway-dotfiles-script
+# Developed and maintained by indicozy
+# ver: 1.0
+
+path=$1			
+theme_folder=$2
+#copy files from $folder/config to .config
+cp -r $path/configs/$theme_folder/config/alacritty ~/.config
+
+# append text to make alacritty config to work with this script
+# this regex is a total piece of shit. change it
+sed -i "s/size: [0-9]\\{1\}\\.[0-9]/$(< $path/customization/alacritty/size.txt)/" ~/.config/alacritty/alacritty.yml
+sed -i "s/size: [0-9]\\{2\}\\.[0-9]/$(< $path/customization/alacritty/size.txt)/" ~/.config/alacritty/alacritty.yml
+sed -i "s/size: [0-9]\\{3\}\\.[0-9]/$(< $path/customization/alacritty/size.txt)/" ~/.config/alacritty/alacritty.yml
